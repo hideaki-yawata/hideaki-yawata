57937afe35005ddd0d3197ade0e6f329f7992842
hideaki-yawata <hideaki.yawata@gmail.com>
2026-09-03 15:10:18 +0200

    update about intro

--- lib/topPageData.ts
+++ lib/topPageData.ts
@@ -1,7 +1,7 @@
 import type { TimelineEntry } from "@/types/work";
 
 export const aboutIntroParagraphs = [
-  "During the COVID-19 pandemic, I began learning web programming independently and later became a freelance web engineer. Over the past 5 years, I have worked on the development, maintenance, and management of numerous websites, primarily using WordPress. I have also explored modern web technologies such as TypeScript, React, and Next.js.",
+  "During the COVID-19 pandemic, I began learning web programming independently and later became a freelance web engineer. Over the past 5 years, I have worked on the development, maintenance, and management of over 100 websites, primarily using WordPress. I have also explored modern web technologies such as TypeScript, React, and Next.js.",
   "In addition to web development, I have experience in web design and photography. This has given me an interest not only in the technical aspects of web development, but also in how information is communicated to users through websites.",
   "Currently based in Berlin, I'm looking for opportunities in web development, web design, or roles that combine both. I hope to contribute my experience to the quality and continuous improvement of websites while working in an international environment.",
 ] as const;
